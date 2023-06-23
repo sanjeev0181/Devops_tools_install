cedc324cb3e6d037fa6448873ac588fe0496fe3b
sanjeev0181 <69623275+sanjeev0181@users.noreply.github.com>
2023-06-23 10:19:54 +0530

    Update jenkins_install

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 sudo apt update
 sudo apt install default-jre -y
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
-sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
-sudo apt install jenkins -y
-sudo systemctl start jenkins.service
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+#sudo apt-get clean
+#sudo apt-get update
